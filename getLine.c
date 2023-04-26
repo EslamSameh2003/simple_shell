a90c161c5d434a0d5c40c2a770b998dec1ced907
EslamSameh2003 <eslamsameh0081@gmail.com>
2023-04-26 11:30:48 +0200

    add

--- getLine.c
+++ getLine.c
@@ -1,74 +1,169 @@
 #include "shell.h"
 
 /**
- * clear_info - initializes inf struct fields
- * @inf:  address of struct
+ * input_buf - buffers Chained commands
+ * @inf: Struct of parameter
+ * @buffer: Address of buffer
+ * @lenv: Address of leng var
+ *
+ * Return: bytes read
  */
-void clear_info(info_t* inf)
+ssize_t input_buf(info_t* inf, char** buffer, size_t* lenv)
 {
-	inf->arg = NULL;
-	inf->argv = NULL;
-	inf->path = NULL;
-	inf->argc = 0;
-}
+	ssize_t x = 0;
+	size_t len_p = 0;
 
-/**
- * set_info - initializes inf struct fields
- * @inf: address of struct
- * @ArgVe: Argument of Vector
- */
-void set_info(info_t* inf, char** ArgVe)
-{
-	int i = 0;
-
-	inf->fname = ArgVe[0];
-	if (inf->arg)
+	if (!*lenv) /* if there is nothing in buffer, fill it */
 	{
-		inf->argv = strtow(inf->arg, " \t  ");
-		if (!inf->argv)
+		free(*buffer);
+		*buffer = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		x = getline(buffer, &len_p, stdin);
+#else
+		x = _getline(inf, buffer, &len_p);
+#endif
+		if (x > 0)
 		{
-
-			inf->argv = malloc(sizeof(char*) * 2);
-			if (inf->argv)
+			if ((*buffer)[x - 1] == '\n')
 			{
-				inf->argv[0] = _strdup(inf->arg);
-				inf->argv[1] = NULL;
+				(*buffer)[x - 1] = '\0'; /* remove trailing newline */
+				x--;
+			}
+			inf->linecount_flag = 1;
+			remove_comments(*buffer);
+			build_history_list(inf, *buffer, inf->histcount++);
+			
+			{
+				*lenv = x;
+				inf->cmd_buf = buffer;
 			}
 		}
-		for (i = 0; inf->argv && inf->argv[i]; i++)
-			;
-		inf->argc = i;
-
-		replace_alias(inf);
-		replace_vars(inf);
 	}
+	return (x);
 }
 
 /**
- * free_info - free inf fields
- * @inf: address of struct
- * @all: true if all ifs are true
+ * get_input - gets a line minus of the newline
+ * @inf: struct of parameter
+ *
+ * Return: bytes read
  */
-void free_info(info_t* inf, int all)
+ssize_t get_input(info_t* inf)
 {
-	ffree(inf->argv);
-	inf->argv = NULL;
-	inf->path = NULL;
-	if (all)
+	static char* buffer; /* the ';' command chain buffer */
+	static size_t y, z, len;
+	ssize_t x = 0;
+	char** buf_poin = &(inf->arg), * poin;
+
+	_putchar(BUF_FLUSH);
+	x = input_buf(inf, &buffer, &len);
+	if (x == -1) /* End of File. */
+		return (-1);
+	if (len)	/* a Commands remain in the chain buffer */
 	{
-		if (!inf->cmd_buf)
-			free(inf->arg);
-		if (inf->env)
-			free_list(&(inf->env));
-		if (inf->history)
-			free_list(&(inf->history));
-		if (inf->alias)
-			free_list(&(inf->alias));
-		ffree(inf->environ);
-		inf->environ = NULL;
-		bfree((void**)inf->cmd_buf);
-		if (inf->readfd > 2)
-			close(inf->readfd);
-		_putchar(BUF_FLUSH);
+		z = y; /* Init new Iterator to current buffer posi */
+		poin = buffer + y; /* Get the pointer to return it */
+
+		check_chain(inf, buffer, &z, y, len);
+		while (z < len) /*Checking the chain  */
+		{
+			if (is_chain(inf, buffer, &z))
+				break;
+			z++;
+		}
+
+		y = z + 1; /* increment  ';'' */
+		if (y >= len) /* reached end of buffer? */
+		{
+			y = len = 0; /*  posi and length = 0 */
+			inf->cmd_buf_type = CMD_NORM;
+		}
+
+		*buf_poin = poin; /* pass back pointer to the current buffer poin position */
+		return (_strlen(poin)); /* return the leng of current command */
 	}
+
+	*buf_poin = buffer; /* get back buffer from _getline() */
+	return (x); /* return leng of buffer  */
+}
+
+/**
+ * read_buf - reads a buffer
+ * @inf: parameter struct
+ * @buffer: buffer
+ * @y: size
+ *
+ * Return: x
+ */
+ssize_t read_buf(info_t* inf, char* buf, size_t* y)
+{
+	ssize_t x = 0;
+
+	if (*y)
+		return (0);
+	x = read(inf->readfd, buf, READ_BUF_SIZE);
+	if (x >= 0)
+		*y = x;
+	return (x);
+}
+
+/**
+ * _getline - gets the next line of input from STDIN
+ * @inf: struct of parameter
+ * @ptr: address of pointer to buffer, preallocated or NULL
+ * @leng: size of preallocated ptr buffer
+ *
+ * Return: L_poin
+ */
+int _getline(info_t* inf, char** ptr, size_t* leng)
+{
+	static char buffer[READ_BUF_SIZE];
+	static size_t y, len;
+	size_t z;
+	ssize_t x = 0, L_poin = 0;
+	char* poin = NULL, * new_poin = NULL, * c;
+
+	poin = *ptr;
+	if (poin && leng)
+		L_poin = *leng;
+	if (y == len)
+		y = len = 0;
+
+	x = read_buf(inf, buffer, &len);
+	if (x == -1 || (x == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buffer + y, '\n');
+	z = c ? 1 + (unsigned int)(c - buffer) : len;
+	new_poin = _realloc(poin, L_poin, L_poin ? L_poin + z : z + 1);
+	if (!new_poin) /* MALLOC FAILURE! */
+		return (poin ? free(poin), -1 : -1);
+
+	if (L_poin)
+		_strncat(new_poin, buffer + y, z - y);
+	else
+		_strncpy(new_poin, buffer + y, z - y + 1);
+
+	L_poin += z - y;
+	y = z;
+	poin = new_poin;
+
+	if (leng)
+		*leng = L_poin;
+	*ptr = poin;
+	return (L_poin);
+}
+
+/**
+ * sigintHandler - prevent ctrl-C
+ * @signal_num: the signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int signal_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
 }
