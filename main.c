a564873d619bd5ed6e1e2037611b36fdd63d27d6
EslamSameh2003 <eslamsameh0081@gmail.com>
2023-04-26 11:06:53 +0200

    add

--- main.c
+++ main.c
@@ -1,33 +1,33 @@
 #include "shell.h"
 
 /**
- * main - Entry point
- * @aC: arg Count
- * @aV: arg Vector
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
  * Return: 0 on success, 1 on error
  */
-int main(int aC, char **aV)
+int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	int f = 2;
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=r" (f)
-		: "r" (f));
+		: "=r" (fd)
+		: "r" (fd));
 
-	if (aC == 2)
+	if (ac == 2)
 	{
-		f = open(aV[1], O_RDONLY);
-		if (f == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
-				_eputs(": 0: Not open ");
+				_eputs(": 0: Can't open ");
 				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
@@ -35,7 +35,7 @@ int main(int aC, char **aV)
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = f;
+		info->readfd = fd;
 	}
 	populate_env_list(info);
 	read_history(info);
